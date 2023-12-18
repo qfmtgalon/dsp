1744284d75b5f058b68ef841bb90f30e83150921
lexiaroque <142638418+lexiaroque@users.noreply.github.com>
2023-12-18 19:51:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,41 +3,36 @@ import librosa
 import librosa.display
 import matplotlib.pyplot as plt
 import numpy as np
-
-# Disable PyplotGlobalUseWarning
-st.set_option('deprecation.showPyplotGlobalUse', False)
+import os
 
 def process_audio_file(uploaded_file):
     # Add your processing logic here
     st.audio(uploaded_file, format='audio/wav')
 
     # Load audio using librosa
-    y, sr = librosa.load(uploaded_file)
+    y, sr = librosa.load(uploaded_file, sr=None)
 
     # Plot waveform
-    plt.figure(figsize=(12, 4))
-    plt.subplot(1, 2, 1)
+    plt.figure(figsize=(8, 4))
     plt.title('Waveform Visualization')
     plt.xlabel('Time')
     plt.ylabel('Amplitude')
     plt.plot(np.arange(len(y)) / sr, y)
+    st.pyplot()
 
-    # Compute mel spectrogram
-    mel_spectrogram = librosa.feature.melspectrogram(y, sr=sr)
+    # Create and display mel spectrogram
+    mel_spectrogram = librosa.feature.melspectrogram(y=y, sr=sr)
     mel_spectrogram_db = librosa.power_to_db(mel_spectrogram, ref=np.max)
 
-    # Plot mel spectrogram
-    plt.subplot(1, 2, 2)
-    librosa.display.specshow(mel_spectrogram_db, x_axis='time', y_axis='mel')
-    plt.colorbar(format='%+2.0f dB')
+    plt.figure(figsize=(10, 4))
+    librosa.display.specshow(mel_spectrogram_db, sr=sr, x_axis='time', y_axis='mel')
     plt.title('Mel Spectrogram')
-    plt.xlabel('Time')
-    plt.ylabel('Mel Frequency')
-    
+    plt.colorbar(format='%+2.0f dB')
+    plt.tight_layout()
     st.pyplot()
 
 def main():
-    st.title('Audio File Uploader and Waveform/Mel Spectrogram Visualizer')
+    st.title('Audio File Uploader and Waveform Visualizer')
 
     uploaded_file = st.file_uploader("Upload an audio file", type=["mp3", "wav", "m4a"])
 
