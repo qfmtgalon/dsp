eb10e08951df24dca61fa1af18c1c267e885668c
lexiaroque <142638418+lexiaroque@users.noreply.github.com>
2023-12-18 22:13:58 +0800

    Update app.py

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ def process_audio_file(uploaded_file):
 
     # Make a prediction
     prediction = model.predict(img_array)
-    prediction_label = 'Real Voice' if prediction[0][0] > 0.009 else 'AI-Generated Voice'
+    prediction_label = 'Real Voice' if prediction[0][0] > prediction[0][1] else 'AI-Generated Voice'
 
     # Display the result
     st.write(f"Prediction: {prediction_label}")
