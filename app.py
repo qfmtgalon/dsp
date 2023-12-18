461be1a5ac1dc8e763d5c19b7e4fe4b0ab5b540a
lexiaroque <142638418+lexiaroque@users.noreply.github.com>
2023-12-18 20:38:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 
+st.set_option('deprecation.showPyplotGlobalUse', False)
+
 def process_audio_file(uploaded_file):
     # Add your processing logic here
     st.audio(uploaded_file, format='audio/wav')
