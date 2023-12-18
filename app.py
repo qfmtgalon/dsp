1e40810c1ca8eb1ffd307a2f5d125e28b79179fd
lexiaroque <142638418+lexiaroque@users.noreply.github.com>
2023-12-18 20:28:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,34 +5,34 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 
+# Function to process the uploaded audio file
 def process_audio_file(uploaded_file):
-    # Add your processing logic here
     st.audio(uploaded_file, format='audio/wav')
 
     # Load audio using librosa
     y, sr = librosa.load(uploaded_file, sr=None)
 
     # Plot waveform
-    plt.figure(figsize=(8, 4))
-    plt.title('Waveform Visualization')
-    plt.xlabel('Time')
-    plt.ylabel('Amplitude')
+    st.header('Waveform Visualization')
+    waveform_chart = st.pyplot(plt.figure(figsize=(8, 4)))
     plt.plot(np.arange(len(y)) / sr, y)
-    st.pyplot()
+    waveform_chart.pyplot()
 
     # Create and display mel spectrogram
+    st.header('Mel Spectrogram')
     mel_spectrogram = librosa.feature.melspectrogram(y=y, sr=sr)
     mel_spectrogram_db = librosa.power_to_db(mel_spectrogram, ref=np.max)
 
-    plt.figure(figsize=(10, 4))
+    mel_spec_chart = st.pyplot(plt.figure(figsize=(10, 4)))
     librosa.display.specshow(mel_spectrogram_db, sr=sr, x_axis='time', y_axis='mel')
-    plt.title('Mel Spectrogram')
     plt.colorbar(format='%+2.0f dB')
-    plt.tight_layout()
-    st.pyplot()
+    mel_spec_chart.pyplot()
 
+# Main function for Streamlit web app
 def main():
+    st.set_page_config(layout="wide", page_title='Audio Analysis App', page_icon='🔊')
     st.title('Classifying Real vs. AI-Generated Voices: A Deep Learning Approach Using Mel Spectrogram Analysis')
+    st.markdown('## Analyze Audio Files')
 
     uploaded_file = st.file_uploader("Upload an audio file", type=["mp3", "wav", "m4a"])
 
