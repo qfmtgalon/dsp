de0a9b616164a8e313a48dd8e59d306882dca8c5
lexiaroque <142638418+lexiaroque@users.noreply.github.com>
2023-12-18 19:03:39 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,35 +2,29 @@ import streamlit as st
 import tensorflow as tf
 import librosa
 import numpy as np
-import cv2
-
+import matplotlib.pyplot as plt
+from PIL import Image
+from io import BytesIO
 
 # Load your trained model
 model = tf.keras.models.load_model('model_scratch.h5')
 
-# Maximum duration for processing
-MAX_DURATION = 6.0
-def preprocess_audio(file):
-    # Load audio file
-    y, sr = librosa.load(file, sr=None, duration=MAX_DURATION)
-
-    # Convert audio signal to spectrogram (adjust parameters as needed)
-    spectrogram = librosa.feature.melspectrogram(y, sr=sr, n_fft=2048, hop_length=512)
-    spectrogram = librosa.power_to_db(spectrogram, ref=np.max)
-
-    # Resize the spectrogram to match the model's input shape
-    resized_spectrogram = cv2.resize(spectrogram, (256, 256), interpolation=cv2.INTER_NEAREST)
-
-    # Expand dimensions to create a batch of size 1
-    processed_audio = np.expand_dims(resized_spectrogram, axis=-1)
-    processed_audio = np.expand_dims(processed_audio, axis=0)
-
-    return processed_audio
-
+def audio_to_spectrogram(audio_file):
+    y, sr = librosa.load(audio_file, sr=None, duration=6.0)
+    S = librosa.feature.melspectrogram(y, sr=sr, n_mels=256, fmax=8000)
+    plt.figure(figsize=(3.2, 3.2))
+    librosa.display.specshow(librosa.power_to_db(S, ref=np.max), fmax=8000)
+    plt.axis('off')
+    buf = BytesIO()
+    plt.savefig(buf, format='png')
+    plt.close()
+    buf.seek(0)
+    img = Image.open(buf)
+    img = img.resize((256, 256))
+    return np.array(img)/255.0
 
 def classify_audio(audio):
-    # Use your model to classify the audio
-    prediction = model.predict(audio)
+    prediction = model.predict(np.expand_dims(audio, axis=0))
     return 'Real Voice' if prediction[0][0] > 0.5 else 'AI-Generated Voice'
 
 st.title('Real Voice vs AI-Generated Voice Classifier')
@@ -38,11 +32,6 @@ st.title('Real Voice vs AI-Generated Voice Classifier')
 uploaded_file = st.file_uploader("Upload an audio file", type=["wav", "mp3"])
 
 if uploaded_file is not None:
-    # Process the audio file
-    processed_audio = preprocess_audio(uploaded_file)
-
-    # Classify the audio
+    processed_audio = audio_to_spectrogram(uploaded_file)
     result = classify_audio(processed_audio)
-
-    # Display the result
     st.write(f'The audio is classified as: {result}')
