d61bdee05cc6a90a29b31bab449d4a85aca33d2d
lexiaroque <142638418+lexiaroque@users.noreply.github.com>
2023-12-18 18:39:54 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,18 @@
 import streamlit as st
-from tempfile import NamedTemporaryFile
-import librosa
 
+def process_audio_file(uploaded_file):
+    # Add your processing logic here
+    st.audio(uploaded_file, format='audio/wav')
 
-def validate_mp3(uploaded_file):
-    try:
-        audio, sr = librosa.load(uploaded_file, sr=None)
-        return True
-    except:
-        return False
+def main():
+    st.title('Audio File Uploader')
 
+    uploaded_file = st.file_uploader("Upload an audio file", type=["mp3", "wav", "m4a"])
 
-st.title("MP3 Player")
+    if uploaded_file is not None:
+        st.write("File uploaded successfully!")
+        if st.button('Process Audio'):
+            process_audio_file(uploaded_file)
 
-uploaded_file = st.file_uploader("Upload your MP3 audio", type=["mp3"])
-
-if uploaded_file:
-    # Validate if it's actually an MP3 file
-    if not validate_mp3(uploaded_file):
-        st.error("Please upload a valid MP3 file.")
-    else:
-        with NamedTemporaryFile(suffix=".mp3") as temp:
-            temp.write(uploaded_file.read())
-            temp.seek(0)
-            st.audio(temp.name, autoplay=False)
-            st.button("Play", on_click=lambda: st.session_state.play_audio)
-            if st.session_state.get("play_audio", False):
-                st.audio(temp.name, format="audio/mpeg", autoplay=True)
-                del st.session_state.play_audio
-
-else:
-    st.write("Please upload an MP3 file to play.")
-
-
+if __name__ == "__main__":
+    main()
